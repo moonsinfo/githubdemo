26be2f47309e9a87b97d057e6591faebcee0015a
Ethan <154518484@qq.com>
2017-10-14 22:56:29 +0800

    master-1

--- src/Test.java
+++ src/Test.java
@@ -6,5 +6,6 @@ public class Test {
     public static void main(String[] args) {
         System.err.println("1");
         System.err.println("master");
+        System.err.println("master-1");
     }
 }
